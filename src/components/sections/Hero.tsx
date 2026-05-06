6db1e5f264dd857ada37e576e6eeba7576b5fd08
OnetADMIN <mirjan01allaraj@gmail.com>
2026-05-06 12:18:30 +0200

    fix

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -68,7 +68,7 @@ export default function Hero({
             </p>
 
             <h1 className="text-4xl md:text-5xl font-extrabold mt-3 leading-tight">
-              Interneti me i shpejtë & stabil,
+              Interneti me ishpejtë & stabil,
               <span className="text-[var(--brand)]"> për familje dhe biznese</span>.
             </h1>
 
