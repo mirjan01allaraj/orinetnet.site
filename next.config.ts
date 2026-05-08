0a32483662bb66e228c4323e1eec87abcb3f6225
OnetADMIN <mirjan01allaraj@gmail.com>
2026-05-08 09:10:22 +0200

    back

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
-  images: {
-    unoptimized: true,
+  images: { unoptimized: true },
+
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://localhost/orientnet-local/api/:path*",
+      },
+    ];
   },
 };
 
